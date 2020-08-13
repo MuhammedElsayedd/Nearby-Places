fab1200c9fb7fe89894ca6bdd90797092781b740
Muhammed Elsayed <muhammedsayedelzayat@gmail.com>
2020-08-14 01:47:49 +0200

    Initial Commit

---
+++ Nearby/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nearby
+//
+//  Created by Muhammed Elsayed on 8/14/20.
+//  Copyright © 2020 Muhammed Elsayed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
