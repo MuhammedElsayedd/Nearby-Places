42176a36eefe9d3c2b2652273b33ef166e3819a2
Muhammed Elsayed <muhammedsayedelzayat@gmail.com>
2020-08-26 18:49:59 +0200

    first commit

---
+++ Nearby/NearbyPlacesResponse.swift
@@ -0,0 +1,9 @@
+//
+//  NearbyPlacesResponse.swift
+//  Nearby
+//
+//  Created by Muhammed Elsayed on 8/14/20.
+//  Copyright © 2020 Muhammed Elsayed. All rights reserved.
+//
+
+import Foundation
---
+++ Nearby/ViewControllerDetails.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerDetails.swift
+//  Nearby
+//
+//  Created by Muhammed Elsayed on 8/19/20.
+//  Copyright © 2020 Muhammed Elsayed. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerDetails: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
